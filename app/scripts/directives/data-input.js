7a78bf230a5e75422bb0142c37a5e96a66db0fd5
Drew Machat <drew@alleyinteractive.com>
2014-08-22 15:46:44 -0400

    add paste input option

--- app/scripts/directives/data-input.js
+++ app/scripts/directives/data-input.js
@@ -18,12 +18,26 @@ define([
             scope.newRow = {};
             scope.newDataFile = {};
 
+            // Set CSV parsing options
+            var csvOptions = {
+              header: true,
+              line: '\n'
+            };
+
+            // Keep an empty new row for the input model
             scope.$watch('structureData.data', function(newval) {
-              angular.forEach(newval, function(val, gidx) {
+              angular.forEach(newval, function(group, gidx) {
                 scope.newRow[gidx] = {};
               });
             }, true);
 
+            // When data is pasted to the input text area, parse it for values
+            scope.onPasteInputChanged = function(input, gidx) {
+              scope.structureData.data[gidx].values = new CSV(input, csvOptions).parse();
+              scope.pasteInputToggle = false;
+            };
+
+            // Append a new data row from the empty data model
             scope.addRow = function(gidx) {
               var validate = true;
               angular.forEach(scope.structureData.columnValues, function(type, key) {
@@ -41,11 +55,10 @@ define([
               scope.structureData.data[gidx].values.splice(idx, 1);
             };
 
-            scope.readFile = function(file, gidx){
-              var csv = new CSV(file, { header: true, line: '\n' }).parse();
-              scope.structureData.data[gidx].values = csv;
+            // This is just the callback from the file reader input. Parse and insert uploaded values
+            scope.readFile = function(file, gidx) {
+              scope.structureData.data[gidx].values = new CSV(file, csvOptions).parse();
             };
-
           }
         };
       });
