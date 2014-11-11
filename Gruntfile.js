7a5c785d5b5b64192482dc676e74761ff1f32501
Drew Machat <drew@alleyinteractive.com>
2014-11-11 01:41:17 -0500

    update dev url and edit-in-place style

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
                 port: 9000,
                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                hostname: 'chartbuilder.dev'
             },
             livereload: {
                 options: {
