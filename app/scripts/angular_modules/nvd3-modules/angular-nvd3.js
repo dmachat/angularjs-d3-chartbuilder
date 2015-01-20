e9b0a1a410e38cfeba9052cb962cb150d9bfefa0
Drew Machat <drew@alleyinteractive.com>
2015-01-19 21:17:16 -0500

    Force wrapLabels

--- app/scripts/angular_modules/nvd3-modules/angular-nvd3.js
+++ app/scripts/angular_modules/nvd3-modules/angular-nvd3.js
@@ -40,6 +40,26 @@ angular
             scope.chart.update();
           },
 
+          // Force labels to wrap
+          wrapLabels: function() {
+            var svgEl = d3.select(element[0]).select('svg');
+
+            if (scope.options.chart.wrapLabels) {
+              svgEl.selectAll('.nv-axis .tick text').each(function(d) {
+                var el = d3.select(this);
+                var words = d.toString().split(' ');
+                el.text('');
+
+                for (var i = 0; i < words.length; i++) {
+                  var tspan = el.append('tspan').text(words[i]);
+                  if (i > 0) {
+                    tspan.attr('x', 0).attr('dy', '15');
+                  }
+                }
+              });
+            }
+          },
+
           // Update chart with new options
           updateWithOptions: function(options) {
 
@@ -155,7 +175,7 @@ angular
               // Update the chart when window resizes
               nv.utils.windowResize(function() { scope.chart.update(); });
               return scope.chart;
-            }, options.chart['callback']);
+            }, scope.api.wrapLabels);
           },
 
           // Update chart with new data
@@ -178,23 +198,9 @@ angular
 
               svgEl[0][0].style.height = scope.options.chart.height + 'px';
               svgEl[0][0].style.width = scope.options.chart.width + 'px';
+
               if (scope.options.chart.type === 'multiChart') {
                 scope.chart.update(); // multiChart is not automatically updated
-              }
-
-              if (scope.options.chart.wrapLabels) {
-                svgEl.selectAll('.nv-axis .tick text').each(function(d) {
-                  var el = d3.select(this);
-                  var words = d.toString().split(' ');
-                  el.text('');
-
-                  for (var i = 0; i < words.length; i++) {
-                    var tspan = el.append('tspan').text(words[i]);
-                    if (i > 0) {
-                      tspan.attr('x', 0).attr('dy', '15');
-                    }
-                  }
-                });
               }
             }
           },
